72b024b5b99130f7838f6534474837d0ac2af81d
Kashish708 <kkashish792004@gmail.com>
2025-07-14 16:47:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WalmartLossPreventionAI from './WalmartLossPreventionAI';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <WalmartLossPreventionAI />
+    </div>
+  );
+}
+
+export default App;
---
+++ WalmartLossPreventionAI.jsx
@@ -0,0 +1,3 @@
+// Replace this with your detailed component (with narrator, AI alerts, live monitoring etc.)
+import React from 'react';
+export default function WalmartLossPreventionAI() { return <div>AI Component Here</div>; }
